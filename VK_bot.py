32686de659b40fe9d32c5a64d9aa64a2678296b3
erool2016 <112575876+erool2016@users.noreply.github.com>
2023-02-26 11:51:44 +0300

    Add files via upload

---
+++ VK_bot.py
@@ -0,0 +1,149 @@
+from random import randrange
+import requests
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from pprint import pprint
+
+# user_id = 87692036
+# user_data = {'id': 87692036, 'domain': 'id87692036', 'city': {'id': 10, 'title': 'Волгоград'}, 'photo_id': '87692036_397545098', 'sex': 1, 'first_name': 'Татьяна', 'can_access_closed': True, 'is_closed': False}
+def get_token():
+    with open('tok111.txt', 'r') as f:
+        return f.readline()
+
+def get_servis_key():
+    with open('token_vkinder_servis_key.txt', 'r') as f:
+        return f.readline()
+
+#token = input('Token: ')
+
+vk = vk_api.VkApi(token=get_servis_key())
+longpoll = VkLongPoll(vk)
+
+#part 2 , 'last_name': 'Демина'
+
+# def get_token():
+#     with open('token111.txt', 'r') as f:
+#         return f.readline()
+def user_search(user_id):
+    ''' Получение данных пользователей для выборки'''
+    URL = 'https://api.vk.com/method/users.search'
+
+    params = {
+
+        'access_token': get_token(),
+        'v': 5.131,
+        'fields': 'bdate,sex,photo_id,about',
+        'count': 10
+    }
+    res = requests.get(URL, params)
+    data = res.json()
+    pprint(data)
+
+    return data
+
+def get_user_info(user_id):
+    '''Собираем и уточняем информацию о пользователе, возвращаем СЛОВАРЬ с информацией'''
+    data_user_for_find = {}
+    not_key = []
+    URL = 'https://api.vk.com/method/users.get'
+    user_info = requests.get(URL,{'access_token': get_token(),'v': 5.131,'user_id':user_id,'fields':'bdate,sex,photo_id,about,city,relation,inerests,domain'}).json()
+    print(user_info)
+    a = user_info['response']
+    # for key, value in q['response']:
+    #     # if key == 'bdate':
+    #     #     data[key] = value
+    #     print(key,value)
+    user_data = a[0]
+    print('user data -', user_data)
+    for key, value in user_data.items():
+
+        if key == 'city':
+            data_user_for_find['city'] = value['id']
+
+        if key == 'bdate':
+            data_user_for_find['bdate'] = value
+        if key == 'sex':
+            data_user_for_find['sex'] = value
+        if key == 'first_name':
+            data_user_for_find['first_name'] = value
+        if key == 'last_name':
+            data_user_for_find['last_name'] = value
+    if 'city' not in data_user_for_find.keys():
+        print('city')
+        not_key.append('city')
+    if 'bdate' not in data_user_for_find.keys():
+        print('bdate')
+        not_key.append('bdate')
+
+    if 'sex' not in data_user_for_find.keys():
+        print('sex')
+        not_key.append('sex')
+
+    # print(data)
+    # pprint(not_key)
+    return data_user_for_find
+
+def get_user_id_from_scren_name(scren_name):
+    # scren_name = who_search()
+    URL = 'https://api.vk.com/method/users.get'
+    user_info = requests.get(URL, {'access_token': get_token(), 'v': 5.131, 'screen_name': scren_name,
+                                   'fields': 'bdate,sex,photo_id,about,city,relation,inerests,domain'}).json()
+    user_id = user_info['response'][0]['id']
+    print(user_id,'----  id  ')
+    return user_id
+
+def who_search():
+    request_user = ''
+    write_msg(user_id,'Для кого будем искать пару ( короткое имя)')
+    # session = vk_api.VkApi(get_servis_key())
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            text = event.text.lower()
+            # user_id = event.user_id
+            request = event.text
+            request_user = request
+            print('requests users - ',request_user)
+            print('искать будем -',request)
+            if type(request_user) == int:
+                return print(request_user,'-----requsts user')
+            print(request_user,'++++++++++')
+            return request_user
+
+def get_user_foto(user_id):
+    session = vk_api.VkApi(token=get_token())
+    response = session.method('photos.get', {
+        'owner_id': user_id,
+        'album_id': 'profile',
+        'extended': 1,
+        'photo_sizes': 1})
+    print(response)
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+session = vk_api.VkApi(get_servis_key())
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        text = event.text.lower()
+        user_id = event.user_id
+
+        # print(user_id)
+
+        if event.to_me:
+            request = event.text
+
+            if request == "привет":
+                write_msg(event.user_id, f"Хай, {event.user_id}")
+                get_user_id_from_scren_name(who_search())
+                get_user_info(user_id)# уточняем, получаем информацию о пользователе
+                get_user_id_from_scren_name(who_search())
+
+                # user_search(user_id)
+                # get_user_foto(user_id)
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
+
+
